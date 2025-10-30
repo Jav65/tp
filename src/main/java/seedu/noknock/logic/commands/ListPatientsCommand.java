44ec22b180885ee430db53c7cdafca293ef40784
Jav65 <j4vierewong0605@gmail.com>
2025-10-30 14:19:49 +0800

    fix: Fix displaying caring sessions after sessions-today commands

--- src/main/java/seedu/noknock/logic/commands/ListPatientsCommand.java
+++ src/main/java/seedu/noknock/logic/commands/ListPatientsCommand.java
@@ -1,9 +1,11 @@
 package seedu.noknock.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.noknock.model.Model.PREDICATE_SHOW_ALL_SESSIONS;
 
 import seedu.noknock.model.Model;
 import seedu.noknock.model.person.IsPatientPredicate;
+import seedu.noknock.model.session.CaringSessionDateInRangePredicate;
 
 /**
  * Lists all patients in the address book to the user.
@@ -18,6 +20,7 @@ public class ListPatientsCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
+        model.setSessionDisplayFilter(PREDICATE_SHOW_ALL_SESSIONS);
         model.updateFilteredPatientList(new IsPatientPredicate());
         return new CommandResult(MESSAGE_SUCCESS);
     }
